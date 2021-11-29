d4c960b27bd40d8ec43a3576a301c0d703de169c
trnhi1010 <trnhi1010@gmail.com>
2021-11-29 21:59:20 +0700

    [Nhi]
    Particle Smoke

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,7 +5,6 @@ using DG.Tweening;
 
 public class CameraFollow : MonoBehaviour
 {
-    //dahkd
     Vector3 desiredPosition;
     public Transform target;
     private Vector3 temp;
@@ -18,11 +17,20 @@ public class CameraFollow : MonoBehaviour
     {
         temp = transform.position;
         temp.z = target.position.z;
-
-        if (Vector3.Distance(temp, target.position) >= 10)
+        
+        if (Vector3.Distance(temp, target.position) >= 5)
         {
-            transform.DOKill();
-            transform.DOMove(target.position + desiredPosition, 0.5f);
+
+            if (target.position.x < -7.2 || target.position.x > 255)
+            {
+                transform.DOKill();
+            }
+            else
+            {
+                transform.DOMoveX(target.position.x + desiredPosition.x ,0.1f);
+                //transform.DOMove(target.position + desiredPosition, 0.1f);
+            }
+            
         }
     }
 }
---
+++ Assets/Scripts/SmokeScripts.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeScripts : MonoBehaviour
+{
+    public GameObject[] smokes;
+    public float speed;
+    public void MoveSmoke()
+    {
+    }    
+}
